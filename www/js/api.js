d511b9a4eddaacba2b3a61802a69aa643c97344a
Ivelina Hristova <iv4etoxx@gmail.com>
2016-01-11 01:41:28 +0200

    update controls

--- www/js/api.js
+++ www/js/api.js
@@ -26,6 +26,66 @@ $(document).ready(function(){
         });
 
 
+    $.ajax({
+        dataType: "json",
+        type: "GET",
+        url: "http://ivelinah.ddns.net/controls",
+        beforeSend: function( xhr ) {
+            xhr.overrideMimeType( "application/json; charset=x-user-defined" );
+        }
+    })
+        .done(function( data ) {
+            var responseObj = data;
+            console.log(responseObj);
+            for(var sensorId in responseObj){
+                var sensor = (responseObj[sensorId]);
+                $('#' + sensor['name'] + ' .control-action').attr('data-status', sensor['status'] == 1 ? 0 : 1);
+                $('#' + sensor['name'] + ' .control-action').attr('data-name', sensor['name']);
+
+                if(sensor['status'] == 1) {
+                    $('#' + sensor['name'] + ' .condition').html('ON');
+                    $('#' + sensor['name'] + ' .condition').addClass('on');
+                    $('#' + sensor['name'] + ' .control-action').html('ЗАТВОРИ');
+                } else {
+                    $('#' + sensor['name'] + ' .condition').html('OFF');
+                    $('#' + sensor['name'] + ' .condition').addClass('off');
+                    $('#' + sensor['name'] + ' .control-action').html('ОТВОРИ');
+                }
+                $('#' + sensor['name'] + '-value').html(sensor['value']);
+                var date = sensor['updated_at'].substring(0, 10);
+                var time = sensor['updated_at'].substring(10, 20);
+                $('#' + sensor['name'] + ' .last-updated-date').html(date);
+                $('#' + sensor['name'] + ' .last-updated-time').html(time);
+            }
+        });
+
+
+    $('.control-action').on('click', function () {
+        console.log(888);
+        var status = $(this).attr('data-status');
+        console.log(status);
+        var name = $(this).attr('data-name');
+
+        $.ajax({
+            dataType: "text",
+            type: "PUT",
+            url: "http://ivelinah.ddns.net/controls/" + name,
+            data: { status: status },
+            beforeSend: function( xhr ) {
+                xhr.overrideMimeType( "application/json; charset=x-user-defined" );
+            },
+            error: function(jq,status,message) {
+                alert('A jQuery error has occurred. Status: ' + status + ' - Message: ' + message);
+            }
+        })
+            .done(function( data ) {
+                console.log(111);
+                console.log(data);
+                location.reload();
+            });
+    });
+
+
    /* var request = new XMLHttpRequest();
     var response = null;
 // Feature detection for CORS
